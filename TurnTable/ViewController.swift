da61a1978da422c114c5f590a251749e9242f11d
Bob Chang <bob910078@gmail.com>
2019-10-10 07:14:10 +0800

    Initial Commit

---
+++ TurnTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TurnTable
+//
+//  Created by Bob Chang on 2019/10/10.
+//  Copyright © 2019 Dandelion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
