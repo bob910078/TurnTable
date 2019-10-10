0e0c6e6a5496240b52beaef9c0ef74e2bcab3a0f
Bob Chang <bob910078@gmail.com>
2019-10-10 17:25:52 +0800

    新增點擊手勢，針對小幅度增減金額使用

--- TurnTable/ViewController.swift
+++ TurnTable/ViewController.swift
@@ -13,7 +13,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var costDisplayLabel: UILabel!
     @IBOutlet weak var turnView: UIView!
     
-    var sum: Float = 100
+    var sum: Float = 100 {
+        didSet {
+            DispatchQueue.main.async {
+                self.costDisplayLabel.text = self.sum.desc
+            }
+        }
+    }
     
     lazy var movement: TurnTableMovementTracker = {
         let rectSize = self.turnView.bounds.size
@@ -24,7 +30,6 @@ class ViewController: UIViewController {
             let newValue = movement.value.rounded(.toNearestOrAwayFromZero)
             guard self.sum + newValue >= 0 else { return }
             self.sum += newValue
-            self.costDisplayLabel.text = self.sum.desc
         }
         return helper
     }()
@@ -36,13 +41,28 @@ class ViewController: UIViewController {
         let panG = UIPanGestureRecognizer(target: self, action: #selector(pan(gesture:)))
         turnView.addGestureRecognizer(panG)
         
+        let tapG = UITapGestureRecognizer(target: self, action: #selector(tap(gesture:)))
+        turnView.addGestureRecognizer(tapG)
+        
         costDisplayLabel.text = self.sum.desc
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let border = max(turnView.bounds.size.width / 2, turnView.bounds.size.height / 2)
+        turnView.layer.cornerRadius = border
     }
     
     @objc func pan(gesture: UIGestureRecognizer) {
         let location = gesture.location(in: self.turnView)
         // print(location)
         movement.touchPoint.value = location
+    }
+    
+    @objc func tap(gesture: UIGestureRecognizer) {
+        let location = gesture.location(in: self.turnView)
+        let isIncreasementArea = location.x > turnView.bounds.width / 2
+        sum += isIncreasementArea ? 1 : -1
     }
     
 }
